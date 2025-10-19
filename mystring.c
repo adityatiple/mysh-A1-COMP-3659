c3026c7a673891ca0ab7305745ea1b5807e8bb7a
Aditya Tiple <atipl367@mtroyal.ca>
2025-10-19 17:58:13 +0000

    completed mystring .h and .c doc&comments

--- mystring.c
+++ mystring.c
@@ -5,11 +5,11 @@
 
 int mystrlen(const char *s)
 {
-  int len = 0;
+    int len = 0;
     while (s[len] != '\0') {
-        len++;
+        len++;                // increment till null is found
     }
-    return len;  
+    return len;               // return total 
 }
 
 int mystrcmp(const char *str1, const char *str2)
@@ -18,7 +18,7 @@ int mystrcmp(const char *str1, const char *str2)
     str1++;
     str2++;
   }   
-   return (char)*str1 - (char)*str2; // if 0 returned both are equal, else not.
+   return (char)*str1 - (char)*str2; // if lexicographically 0 returned both are equal, else not.
 }
 
 char *mystrcpy(char *dest, const char *src)
@@ -29,31 +29,31 @@ char *mystrcpy(char *dest, const char *src)
         d++;
         src++;
     }
-    *d = '\0';             // add the final null terminator
+    *d = '\0';             // add the null terminator
     return dest;           // return original dest pointer
 }
 
 char *mystrdup(const char *begin, const char *end) {
     int len = (int)(end - begin);
-    char *new_str = alloc(len + 1);
+    char *new_str = alloc(len + 1);   // dynamically allocate enough memory of the new_str len + '\0'
 
     if (!new_str) {
       return NULL;
     }
-    for (int i = 0; i < len; i++) {
+    for (int i = 0; i < len; i++) {   // duplicate chars from substring range
       new_str[i] = begin[i];      
     }
-    new_str[len] = '\0';    
-    return new_str;
+    new_str[len] = '\0';              // add the null terminator
+    return new_str;                   // return new duplicated string
 }
 
 char *mystrcat(char *dest, const char *src) {
-    int dlen = mystrlen(dest);
+    int d_len = mystrlen(dest);     
     int i = 0;   
 
-   for (i; src[i] != '\0'; i++){
-    dest[dlen + i] = src[i];
-   }
-    dest[dlen + i] = '\0';
-    return dest;
+    for (i; src[i] != '\0'; i++){
+      dest[d_len + i] = src[i];     // start appending src string on dest's '\0' index 
+    }
+    dest[d_len + i] = '\0';         // add the null terminator 
+    return dest;                    // retrun concatenated string 
 }
--- mystring.h
+++ mystring.h
@@ -10,9 +10,65 @@ mystrlen: Function calculates the length of a string by counting the number
  @return The number of characters in the string (excluding the null terminator).
 */
 int mystrlen(const char *str);
-int mystrcmp(const char *s1, const char *s2);
+
+/*
+mystrcmp: Function compares two strings lexicographically (character by character). 
+          It iterates through both strings simultaneously until a differing character 
+          is found or until the end of either string ('\0') is reached. 
+
+          The comparison is based on the ASCII values of the characters at the point 
+          of difference. This behavior matches that of the standard strcmp() function.
+
+ @param str1: Pointer to the first null-terminated string.
+ @param str2: Pointer to the second null-terminated string.
+
+ @return  0  if both strings are identical,
+         <0 if str1 is lexicographically less than str2,
+         >0 if str1 is lexicographically greater than str2.
+*/
+int mystrcmp(const char *str1, const char *str2);
+
+/*
+mystrcpy: Function copies the contents of a source string into a destination buffer.
+          Each character from 'src' is copied sequentially into 'dest' until the
+          null terminator ('\0') is reached. The destination string is null-terminated
+          after copying is complete.
+
+ @param dest: Pointer to the destination buffer where the string will be copied.
+ @param src:  Pointer to the source null-terminated string to be copied.
+
+ @return A pointer to the destination buffer (dest).
+*/
 char *mystrcpy(char *dest, const char *src);
+
+/*
+mystrdup: Function duplicates a substring from a given range of memory defined by
+          'begin' and 'end'. It dynamically allocates a new buffer large enough
+          to hold the copied characters plus a null terminator ('\0').
+
+          The characters between 'begin' (inclusive) and 'end' (exclusive)
+          are copied into the newly allocated buffer, and a null terminator
+          is appended at the end to form a valid C-string.
+
+ @param begin: Pointer to the start of the substring to duplicate.
+ @param end:   Pointer to one position past the last character to copy.
+
+ @return Pointer to the newly allocated null-terminated string,
+         or NULL if memory allocation fails.
+*/
 char *mystrdup(const char *begin, const char *end);
+
+/*
+mystrcat: Function appends the contents of the source string ('src') to the end of 
+          the destination string ('dest'). It first finds the length of 'dest' using 
+          mystrlen(), then copies each character from 'src' starting at that position. 
+          A null terminator ('\0') is added at the end to complete the new string.
+
+ @param dest: Pointer to the destination string where 'src' will be appended.
+ @param src:  Pointer to the source null-terminated string to append.
+
+ @return Pointer to the destination string containing the concatenated result.
+*/
 char *mystrcat(char *dest, const char *src);
 
 #endif
